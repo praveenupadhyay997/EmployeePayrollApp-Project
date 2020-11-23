8449b5f8ca2013d022497affcf4a229f0fc66a46
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-11-23 22:43:04 +0530

    [Praveen] Add: Implemented the date and name validation feature for the dynamic data by the user.

--- js/EmployeePayrollClass.js
+++ js/EmployeePayrollClass.js
@@ -19,7 +19,7 @@ class EmployeePayRoll {
   set name(name) {
     let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z]{2,}$");
     if (nameRegex.test(name)) this._name = name;
-    else throw "Name is Incorrect!";
+    else throw "Incorect Name!!!";
   }
 
   get profilePic() {
@@ -56,19 +56,18 @@ class EmployeePayRoll {
   set note(note) {
     this._note = note;
   }
+  /// Defining the validation for the start date whether it's a future date or not
+  get startDate() { return this._startDate; }
+  set startDate(startDate) { 
+   let now = new Date();
+   if (startDate > now) throw 'Start Date is a Future Date!';
+   var diff = Math.abs(now.getTime() - startDate.getTime());
+   if (diff / (1000 * 60 * 60 * 24) > 30) 
+     throw 'Start Date is beyond 30 Days!';
+   this._startDate = startDate; 
+ }
 
-  get startDate() {
-    return this._startDate;
-  }
-  set startDate(startDate) {
-    let currentDate= new Date();
-    /// Checking if the date entered by the user is future date or not
-        if(currentDate- startDate>=0)
-            this._startDate= startDate;
-        else throw  'invalid date';
-  }
-
-  // defining toString method for aggregrating the data entered by the user
+  /// Defining toString method for aggregrating the data entered by the user
   toString() {
     const options = { year: "numeric", month: "short", day: "numeric" };
     const empDate = !this.startDate ? "undefined" : this.startDate;
